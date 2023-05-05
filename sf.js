21da90a21e372bf17633670956148e787e9f54cf
Chris Solorzano <chrisxsolo177@gmail.com>
2023-05-05 16:51:17 -0700

    updated chinatown

--- sf.js
+++ sf.js
@@ -1,55 +1,29 @@
-const openEls = document.querySelectorAll("[data-open]");
-const closeEls = document.querySelectorAll("[data-close]");
-const isVisible = "is-visible";
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-for (const el of openEls) {
-  el.addEventListener("click", function() {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-  });
-}
-
-for (const el of closeEls) {
-  el.addEventListener("click", function() {
-    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-  });
-}
-
-document.addEventListener("click", e => {
-  if (e.target == document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+document.addEventListener("DOMContentLoaded", function() {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modalImg");
+  
+  var img1 = document.getElementById("img1");
+  var img2 = document.getElementById("img2");
+  var img3 = document.getElementById("img3");
+  
+  img1.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+  
+  img2.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+  
+  img3.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+  
+  var span = document.getElementsByClassName("close")[0];
+  
+  span.onclick = function() {
+    modal.style.display = "none";
   }
 });
-
-document.addEventListener("keyup", e => {
-  // if we press the ESC
-  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
-  }
-});
-
-overlay.addEventListener('click', () => {
-	const modals = document.querySelectorAll('.modal.active')
-	modals.forEach(modal => {
-	  closeModal(modal)
-	})
-  })
-  
-  closeModalButtons.forEach(button => {
-	button.addEventListener('click', () => {
-	  const modal = button.closest('.modal')
-	  closeModal(modal)
-	})
-  })
-  
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
